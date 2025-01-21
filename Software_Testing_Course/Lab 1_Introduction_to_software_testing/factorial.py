110bb1b036856789dcc505c5627e7d107970e67a
Abdiaziz <152752168+Abdiaziz-Ibrahim-Adam@users.noreply.github.com>
2025-01-21 13:55:18 +0100

    Delete Software_Testing_Course directory

--- Software_Testing_Course/Lab 1_Introduction_to_software_testing/factorial.py
+++
@@ -1,8 +0,0 @@
-# factorial.py
-
-# This function calculates the factorial of a number
-def calculate_factorial(n):
-    if n == 0 or n == 1:
-        return 1
-    else:
-        return n * calculate_factorial(n - 1)
